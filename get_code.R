86c3bb76e1da0b25eb4832e89d8f33585f31259b
Jonathan Polonsky <jonny.polonsky@gmail.com>
2015-02-23 22:39:25 +0100

    blah

--- get_code.R
+++ get_code.R
@@ -9,5 +9,5 @@ source_github <- function(u) {
 
 source_github("https://raw.githubusercontent.com/jpolonsky/erm_finan_rep/master/my_functions.R")
 source_github("https://raw.githubusercontent.com/jpolonsky/erm_finan_rep/master/L3_emergencies_2015.R")
-#source_github("https://raw.githubusercontent.com/jpolonsky/erm_finan_rep/master/test.R")
+source_github("https://raw.githubusercontent.com/jpolonsky/erm_finan_rep/master/test.R")
 #source_github("https://raw.githubusercontent.com/jpolonsky/erm_finan_rep/master/get_code.R")
