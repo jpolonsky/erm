8a21de2a13f4f31d0bafba5c2eb4f7abd21757f2
jpolonsky <jonny.polonsky@gmail.com>
2015-02-23 16:30:09 +0100

    Add overall code

---
+++ get_code.R
@@ -0,0 +1,12 @@
+source_github <- function(u) {
+    # load package
+    require(RCurl)
+    
+    # read script lines from website and evaluate
+    script <- getURL(u, ssl.verifypeer = FALSE)
+    eval(parse(text = script), envir = .GlobalEnv)
+}  
+
+source_github("https://raw.githubusercontent.com/jpolonsky/erm_finan_rep/master/my_functions.R")
+source_github("https://raw.githubusercontent.com/jpolonsky/erm_finan_rep/master/L3_emergencies_2015.R")
+source_github("https://raw.githubusercontent.com/jpolonsky/erm_finan_rep/master/test.R")
