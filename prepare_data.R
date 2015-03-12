5cf42f2c66ca747576b68216ebc8938b74ba2d34
jpolonsky <jonny.polonsky@gmail.com>
2015-03-12 10:03:48 +0100

    Multiple changes to allow for Microsoft installation and separate offline/online versions

--- prepare_data.R
+++ prepare_data.R
@@ -7,7 +7,10 @@ if(!require(scales)) {install.packages("scales"); require(scales)}
 if(!require(RColorBrewer)) {install.packages("RColorBrewer"); require(RColorBrewer)}
 #if(!require(extrafont)) {install.packages("extrafont"); require(extrafont)}
 
-wb <- loadWorkbook('./2012 pledges and contributions.xlsx', create = F)
+# wb <- loadWorkbook('K:/clData/ERM_Financial_Tool/2012 pledges and contributions.xlsx', create = F)
+filename <- list.files(path = '.', pattern = "xlsx")
+# wb <- loadWorkbook('./2012 pledges and contributions.xlsx', create = F)
+wb <- loadWorkbook(filename, create = F)
 df_raw <- readWorksheet(wb, sheet = "Contribution data", startRow = 2)
 df_extra <- readWorksheet(wb, sheet = "Soft pledges-other ctrbns 2015", startRow = 2)
 df_filter <- readWorksheet(wb, sheet = "SRP 2015 funds requested", startRow = 2)
@@ -42,27 +45,27 @@ list_donor <- sort(unique(df$donor))
 list_appeal <- sort(unique(df$appeal))
 
 df_donor <- 
-    df %>%
-    #filter(status == 'L3' | 
-    #           status == 'priority') %>%
-    #group_by(country, appeal, status, donor) %>%
-    group_by(appeal, status, donor) %>%
-    summarise(total_requested = min(amount_requested, na.rm = T),
-              total_received = sum(amount_received, na.rm = T),
-              prop_funded = round(sum(amount_received, na.rm = T)/min(amount_requested, na.rm = T)*100, digits = 1)) %>%
-    arrange(status, appeal, desc(total_received))
+  df %>%
+  #filter(status == 'L3' | 
+  #           status == 'priority') %>%
+  #group_by(country, appeal, status, donor) %>%
+  group_by(appeal, status, donor) %>%
+  summarise(total_requested = min(amount_requested, na.rm = T),
+            total_received = sum(amount_received, na.rm = T),
+            prop_funded = round(sum(amount_received, na.rm = T)/min(amount_requested, na.rm = T)*100, digits = 1)) %>%
+  arrange(status, appeal, desc(total_received))
 
 df_donor$prop_funded <- ifelse(df_donor$prop_funded == Inf, 100, df_donor$prop_funded)
 df_donor$prop_funded[is.na(df_donor$prop_funded)] <- 0
 
 df_total <- 
-    df %>%
-    #group_by(country, appeal, status) %>%
-    group_by(appeal, status) %>%
-    summarise(total_requested = min(amount_requested, na.rm = T),
-              total_received = sum(amount_received, na.rm = T),
-              prop_funded = round(sum(amount_received, na.rm = T)/min(amount_requested, na.rm = T)*100, digits = 1)) %>%
-    arrange(desc(prop_funded), status, appeal)
+  df %>%
+  #group_by(country, appeal, status) %>%
+  group_by(appeal, status) %>%
+  summarise(total_requested = min(amount_requested, na.rm = T),
+            total_received = sum(amount_received, na.rm = T),
+            prop_funded = round(sum(amount_received, na.rm = T)/min(amount_requested, na.rm = T)*100, digits = 1)) %>%
+  arrange(desc(prop_funded), status, appeal)
 
 df_total$prop_funded <- ifelse(df_total$prop_funded == Inf, 100, df_total$prop_funded)
 df_total$prop_funded[is.na(df_total$prop_funded)] <- 0
@@ -78,34 +81,34 @@ blues_func <- colorRampPalette(brewer.pal(9, 'Blues'))
 red_func <- colorRampPalette(brewer.pal(9, 'Reds'))
 
 func_pie <- function(x, appeal) {
-    x <- x[x$appeal == appeal, ]
-    #x <- df_donor[df_donor$appeal == 'Central African Republic: SRP 2014', ]
-    
-    x_melt <- melt(x, id = c('appeal', 'status', 'donor', 'total_requested'))
-    
-    dat <- x_melt %>%
-        mutate(pos = cumsum(value) - 0.5*value)
-    
-    dat$donor <- factor(dat$donor, levels = dat$donor)
-    
-    ggplot(dat, aes(x = '', y = value, order = -value, fill = donor)) + 
-        geom_bar(width = 1, stat = 'identity', colour = 'white') + 
-        coord_polar('y', start = 0) + 
-        #scale_fill_discrete() +
-        scale_fill_manual(values = blues_func(nrow(dat)/2)) +
-        #guides(fill = guide_legend(title = 'Donor', ncol = 2)) +
-        theme_bw() +
-        theme(panel.border = element_blank(),
-              plot.title = element_text(size = 12, face = 'bold', color = 'darkblue'), 
-              #plot.title = element_text(family = 'Calibri', size = 12, face = 'bold', color = 'darkblue'), 
-              legend.key = element_blank(),
-              legend.position = '',
-              axis.ticks = element_blank(),
-              axis.text = element_blank(),
-              panel.grid  = element_blank()) +
-        labs(title = paste0('Firm pledges/contributions received\nby WHO: US$', comma(sum(x_melt$value, na.rm = T))), 
-             x = '', y = '') 
-    
+  x <- x[x$appeal == appeal, ]
+  #x <- df_donor[df_donor$appeal == 'Central African Republic: SRP 2014', ]
+  
+  x_melt <- melt(x, id = c('appeal', 'status', 'donor', 'total_requested'))
+  
+  dat <- x_melt %>%
+    mutate(pos = cumsum(value) - 0.5*value)
+  
+  dat$donor <- factor(dat$donor, levels = dat$donor)
+  
+  ggplot(dat, aes(x = '', y = value, order = -value, fill = donor)) + 
+    geom_bar(width = 1, stat = 'identity', colour = 'white') + 
+    coord_polar('y', start = 0) + 
+    #scale_fill_discrete() +
+    scale_fill_manual(values = blues_func(nrow(dat)/2)) +
+    #guides(fill = guide_legend(title = 'Donor', ncol = 2)) +
+    theme_bw() +
+    theme(panel.border = element_blank(),
+          plot.title = element_text(size = 12, face = 'bold', color = 'darkblue'), 
+          #plot.title = element_text(family = 'Calibri', size = 12, face = 'bold', color = 'darkblue'), 
+          legend.key = element_blank(),
+          legend.position = '',
+          axis.ticks = element_blank(),
+          axis.text = element_blank(),
+          panel.grid  = element_blank()) +
+    labs(title = paste0('Firm pledges/contributions received\nby WHO: US$', comma(sum(x_melt$value, na.rm = T))), 
+         x = '', y = '') 
+  
 }
 
 #func_pie(df_donor, list_appeal[2])
@@ -123,44 +126,43 @@ func_pie <- function(x, appeal) {
 df_total$appeal <- factor(df_total$appeal, levels = df_total$appeal)
 
 wind_chart <- 
-    ggplot(df_total, aes(x = appeal, y = prop_funded, order = -prop_funded, fill = appeal)) + 
-    geom_bar(width = 1, stat = 'identity', colour = 'white') + 
-    coord_polar("y", start = 0) + 
-    scale_fill_manual(values = blues_func(nrow(df_total))) +
-    theme_bw() +
-    #guides(fill = guide_legend(title = 'Donor', ncol = 2)) +
-    theme(panel.border = element_blank(),
-          plot.title = element_text(size = 12, face = 'bold', color = 'darkblue'), 
-          #plot.title = element_text(family = 'Calibri', size = 12, face = 'bold', color = 'darkblue'), 
-          legend.key = element_blank(),
-          #legend.position = '',
-          axis.ticks = element_blank(),
-          axis.text = element_blank(),
-          panel.grid  = element_blank()) +
-    labs(title = 'Proportion of total amount requested\nalready contributed or firmly pledged, by appeal', 
-         x = '', y = '') 
+  ggplot(df_total, aes(x = appeal, y = prop_funded, order = -prop_funded, fill = appeal)) + 
+  geom_bar(width = 1, stat = 'identity', colour = 'white') + 
+  coord_polar("y", start = 0) + 
+  scale_fill_manual(values = blues_func(nrow(df_total))) +
+  theme_bw() +
+  #guides(fill = guide_legend(title = 'Donor', ncol = 2)) +
+  theme(panel.border = element_blank(),
+        plot.title = element_text(size = 12, face = 'bold', color = 'darkblue'), 
+        #plot.title = element_text(family = 'Calibri', size = 12, face = 'bold', color = 'darkblue'), 
+        legend.key = element_blank(),
+        #legend.position = '',
+        axis.ticks = element_blank(),
+        axis.text = element_blank(),
+        panel.grid  = element_blank()) +
+  labs(title = 'Proportion of total amount requested\nalready contributed or firmly pledged, by appeal', 
+       x = '', y = '') 
 
 #ggsave(file = 'fig_wind_chart.pdf', width = 6, height = 4)
 
 bar_chart <- 
-    #ggplot(df_total, aes(x = appeal, y = prop_funded, order = -prop_funded, fill = appeal)) + 
-    ggplot(df_total, aes(x = appeal, y = prop_funded, fill = appeal)) + 
-    geom_bar(stat = 'identity', colour = 'white') + 
-    scale_fill_manual(values = blues_func(nrow(df_total))) +
-    #theme_bw() +
-    theme(panel.border = element_blank(),
-          plot.title = element_text(size = 12, face = 'bold', color = 'darkblue'), 
-          #plot.title = element_text(family = 'Calibri', size = 12, face = 'bold', color = 'darkblue'), 
-          legend.key = element_blank(),
-          legend.position = '',
-          axis.text.x = element_text(size = 7, angle = 45, hjust = 1, colour = 'black'),
-          #axis.text = element_blank(),
-          #panel.grid  = element_blank(),
-          axis.ticks = element_blank()) +
-    labs(title = '', x = '', y = 'Funded (%)') 
+  #ggplot(df_total, aes(x = appeal, y = prop_funded, order = -prop_funded, fill = appeal)) + 
+  ggplot(df_total, aes(x = appeal, y = prop_funded, fill = appeal)) + 
+  geom_bar(stat = 'identity', colour = 'white') + 
+  scale_fill_manual(values = blues_func(nrow(df_total))) +
+  #theme_bw() +
+  theme(panel.border = element_blank(),
+        plot.title = element_text(size = 12, face = 'bold', color = 'darkblue'), 
+        #plot.title = element_text(family = 'Calibri', size = 12, face = 'bold', color = 'darkblue'), 
+        legend.key = element_blank(),
+        legend.position = '',
+        axis.text.x = element_text(size = 7, angle = 45, hjust = 1, colour = 'black'),
+        #axis.text = element_blank(),
+        #panel.grid  = element_blank(),
+        axis.ticks = element_blank()) +
+  labs(title = '', x = '', y = 'Funded (%)') 
 
 #ggsave(file = 'fig_bar_chart.pdf', width = 12, height = 8)
 
 
 # save.image("./2.analysed2015.RData")
-
