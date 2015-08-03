f89d0bc8f9f0748c0cdd797cd1f5414dc97a77a2
jpolonsky <jonny.polonsky@gmail.com>
2015-08-03 11:39:02 +0200

    Create prep.R

---
+++ prep.R
@@ -0,0 +1,87 @@
+inFile <- input$data
+    
+    if (is.null(inFile)) return(NULL)
+    
+    wb <- loadWorkbook(inFile$datapath, create = F)
+    listSheets <- getSheets(wb)
+    df_raw <- readWorksheet(wb, sheet = listSheets[grep("Contribution", listSheets)], startRow = 2, check.names = F)
+    df_extra <- readWorksheet(wb, sheet = listSheets[grep(paste0("ctrbns ", input$year), listSheets)], startRow = 2, check.names = F)
+    df_filter <- readWorksheet(wb, sheet = listSheets[grep(paste0("SRP ", input$year), listSheets)], startRow = 3, check.names = F)
+    
+    #     file.rename(inFile$datapath, paste(inFile$datapath, ".xlsx", sep=""))
+    #     listSheets <- excel_sheets(inFile$datapath)
+    #     df_raw <- read_excel(paste0(inFile$datapath, ".xlsx"), sheet = listSheets[grep("Contribution", listSheets)], skip = 1)
+    #     df_extra <- read_excel(paste0(inFile$datapath, ".xlsx"), sheet = listSheets[grep(paste0("ctrbns ", year), listSheets)], skip = 1)
+    #     df_filter <- read_excel(paste0(inFile$datapath, ".xlsx"), sheet = listSheets[grep("SRP", listSheets)], skip = 2)
+    
+    df_raw <- excludeEmpty(df_raw)
+    df_extra <- excludeEmpty(df_extra)
+    df_filter <- excludeEmpty(df_filter)
+    
+    ## Create lists of variables of interest
+    list_var <- c('Recipient country', 'Donor', 'Type of appeal issued', 'Amount in US$')
+    
+    ## Restrict dataframe to variables of interest
+    df_selectionx <- df_raw[, list_var]
+    names(df_selectionx) <- c('country', 'donor', 'appeal', 'amount_received')
+    
+    if (df_extra$Status %in% 'Contribution' %>% grep(T, .) %>% length(.)) {
+      
+      df_extra <- df_extra[df_extra$Status %in% 'Contribution', c(3, 5, 7, 9)]
+      names(df_extra) <- c('country', 'donor', 'appeal', 'amount_received')
+      
+      df_selection <- rbind(df_selectionx, df_extra)
+      
+    } else {
+      
+      df_selection <- df_selectionx
+      
+    }
+    
+    names(df_filter) <- c('appeal', 'status', 'amount_requested')
+    
+    ## Merge datasets to restrict to appeals of interest
+    df <- merge(df_filter, df_selection)
+    
+    list_status <- c('L3', 'L2', 'Priority', 'Other') %>% factor(.)
+    list_country <- df$country %>% unique(.) %>% sort(.)
+    list_donor <- df$donor %>% unique(.) %>% sort(.)
+    list_appeal <- df$appeal %>% unique(.) %>% sort(.)
+    
+    df_donor <-
+      df %>%
+      group_by(appeal, status, donor) %>%
+      summarise(total_requested = min(amount_requested, na.rm = T),
+                total_received = sum(amount_received, na.rm = T),
+                prop_funded = round(sum(amount_received, na.rm = T)/min(amount_requested, na.rm = T)*100, digits = 1)) %>%
+      arrange(status, appeal, desc(total_received))
+    
+    df_donor$prop_funded <- ifelse(df_donor$prop_funded == Inf, 100, df_donor$prop_funded)
+    df_donor$prop_funded[is.na(df_donor$prop_funded)] <- 0
+    
+    df_total <-
+      df %>%
+      group_by(appeal, status) %>%
+      summarise(total_requested = min(amount_requested, na.rm = T),
+                total_received = sum(amount_received, na.rm = T),
+                prop_funded = round(sum(amount_received, na.rm = T)/min(amount_requested, na.rm = T)*100, digits = 1)) %>%
+      arrange(desc(prop_funded), status, appeal)
+    
+    df_total$prop_funded <- ifelse(df_total$prop_funded == Inf, 100, df_total$prop_funded)
+    df_total$prop_funded[is.na(df_total$prop_funded)] <- 0
+    
+    df_total$status <- factor(df_total$status, levels = c('L3', 'L2', 'Priority', 'Other'))
+    df_total <- arrange(df_total, desc(prop_funded))
+    df_total <- df_total %>% filter(!is.na(status))
+    
+    list_appeals_L3 <- sort(unique(df_total$appeal[df_total$status == 'L3']))
+    list_appeals_priority <- sort(unique(df_total$appeal[df_total$status == 'Priority']))
+    list_appeals_other <- sort(unique(df_total$appeal[df_total$status == 'Other']))
+    
+    df_total$appeal <- factor(df_total$appeal, levels = df_total$appeal)
+    
+    df_total$status <- factor(df_total$status, levels = c('L3', 'L2', 'Priority', 'Other'))
+    df_total <- df_total[order(df_total$status, desc(df_total$prop_funded)), ]
+    names(df_total) <- c('Appeal', 'Crisis type', 'Amount requested', 'Amount received', 'Funded (%)')
+    
+    return(list(df_total, df_donor, df_filter))
